f51396b924252b3729b95dfa98cd052aeb53324c
Xi-Plus <a2093064@yahoo.com.tw>
2015-01-14 20:45:37 +0800

    [SE] more color money

--- func/color_money.php
+++ func/color_money.php
@@ -1,8 +1,9 @@
 <?php
 function color_money($money){
 	if($money>=250)return "<font color='#66FFE6'>".$money."</font>";
-	else if($money>=100)return "<font color='#66FF00'>".$money."</font>";//return "#66FF00";
-	else if($money>=0)return "<font color='#FFFF00'>".$money."</font>";//return "#FFFF00";
-	else return "<font color='#FF0000'>".$money."</font>";//"#FF0000";
+	else if($money>=100)return "<font color='#66FF00'>".$money."</font>";
+	else if($money>=0)return "<font color='#FFFF00'>".$money."</font>";
+	else if($money>-100)return "<font color='#FFA500'>".$money."</font>";
+	else return "<font class='blink' color='#FF0000'>".$money."</font>";
 }
 ?>
